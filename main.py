c635bc476002f0dafd47ce8115c489eb9f1b8cd2
Matti Pulkkinen <133065141+mattipulkkinen@users.noreply.github.com>
2024-03-04 20:29:50 +0200

    Finish project

--- main.py
+++ main.py
@@ -1,16 +1,36 @@
 def main():
     path_to_book = "books/frankenstein.txt"
-    with open(path_to_book) as f:
-        file_contents = f.read()
-        word_count = get_word_count(file_contents)
-        print(f"The book contains {word_count} words")
-        character_count = get_character_count(file_contents)
-        print(f"The book contains {character_count}")
+    file_contents = read_file(path_to_book)
+
+    word_count = get_word_count(file_contents)
+
+    character_counts = get_character_counts(file_contents)
+    character_frequencies = to_list_of_dicts(character_counts)
+    character_frequencies.sort(reverse=True, key=sort_on)
+
+    print_report(path_to_book, word_count, character_frequencies)
+
+
+def print_report(path_to_book, word_count, character_frequencies):
+    print(f"Reporting on {path_to_book}")
+    print(f"The file contains {word_count} words.")
+    print()
+    for freq in character_frequencies:
+        character = freq["char"]
+        count = freq["count"]
+        print(f"The '{character}' character is found {count} times")
+
+
+def read_file(file_path):
+    with open(file_path) as f:
+        return f.read()
+
 
 def get_word_count(file_contents):
     return len(file_contents.split())
 
-def get_character_count(file_contents):
+
+def get_character_counts(file_contents):
     counts = {}
     for character in file_contents.lower():
         if character not in counts:
@@ -19,6 +39,19 @@ def get_character_count(file_contents):
         counts[character] += 1
 
     return counts
+
+
+def to_list_of_dicts(dict):
+    result = []
+    for key in dict:
+        if key.isalpha():
+            result.append({"char": key, "count": dict[key]})
+    return result
+
+
+def sort_on(dict):
+    return dict["count"]
+
 
 if __name__ == "__main__":
     main()
